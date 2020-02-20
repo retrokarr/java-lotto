e5d447886c571db48a0f52ce0d0b8686fcba3f5b
김광현 <greentea@zuminternet.com>
2020-02-20 14:35:45 +0900

    refactor : apply enum map

--- src/main/java/lotto/prize/LottoPrize.java
+++ src/main/java/lotto/prize/LottoPrize.java
@@ -1,6 +1,10 @@
 package lotto.prize;
 
-import static lotto.util.LottoStatics.NUMBER_OF_LOTTO_NUMBER;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum LottoPrize {
     FIRST_PRIZE(6, 2000000000),
@@ -8,6 +12,11 @@ public enum LottoPrize {
     THIRD_PRIZE(4, 50000),
     FOURTH_PRIZE(3, 5000),
     NONE(0, 0);
+
+    private static final Map<Integer, LottoPrize> prizes = Collections.unmodifiableMap(
+            Stream.of(values())
+                    .collect(Collectors.toMap(LottoPrize::getMatchCount, Function.identity()))
+    );
 
     private int prizeMoney;
     private int matchCount;
@@ -18,11 +27,7 @@ public enum LottoPrize {
     }
 
     public static LottoPrize ofMatchCount(int matchCount) {
-        if(NUMBER_OF_LOTTO_NUMBER - matchCount > FOURTH_PRIZE.ordinal()) {
-            return NONE;
-        }
-
-        return LottoPrize.values()[NUMBER_OF_LOTTO_NUMBER - matchCount];
+        return prizes.getOrDefault(matchCount, NONE);
     }
 
     public int getPrizeMoney() {
