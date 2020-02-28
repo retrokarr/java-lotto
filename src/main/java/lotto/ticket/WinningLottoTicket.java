8e4c6a6e5f07575293b17f8652d8f78bf7887fda
nokchax <nokchax@gmail.com>
2020-02-28 10:12:42 +0900

    refactor : input range check and extract method

--- src/main/java/lotto/ticket/WinningLottoTicket.java
+++ src/main/java/lotto/ticket/WinningLottoTicket.java
@@ -3,15 +3,16 @@ package lotto.ticket;
 import java.util.Collections;
 import java.util.List;
 
+import static lotto.util.LottoStatics.LIMIT_OF_LOTTO_NUMBER;
+import static lotto.util.LottoStatics.START_OF_LOTTO_NUMBER;
+
 public class WinningLottoTicket extends LottoTicket {
     private int bonusNumber;
 
     public WinningLottoTicket(List<Integer> numbers, int bonusNumber) {
         super(numbers);
 
-        if(numbers.contains(bonusNumber)) {
-            throw new IllegalArgumentException("Duplicated number");
-        }
+        validate(numbers, bonusNumber);
 
         this.bonusNumber = bonusNumber;
     }
@@ -22,5 +23,23 @@ public class WinningLottoTicket extends LottoTicket {
 
     public int getBonusNumber() {
         return bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        rangeCheckOfBonusNumber(bonusNumber);
+
+        duplicationCheckOfBonusNumber(numbers, bonusNumber);
+    }
+
+    private void duplicationCheckOfBonusNumber(List<Integer> numbers, int bonusNumber) {
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("Duplicated number");
+        }
+    }
+
+    private void rangeCheckOfBonusNumber(int bonusNumber) {
+        if(bonusNumber < START_OF_LOTTO_NUMBER || bonusNumber > LIMIT_OF_LOTTO_NUMBER) {
+            throw new IllegalArgumentException("Wrong input of bonus number : [" + bonusNumber + "]");
+        }
     }
 }
